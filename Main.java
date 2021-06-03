2d344990c2e70a11943184be85eb9a5c7bb8e262
shqiperonsadiku <74976264+shqiperonsadiku@users.noreply.github.com>
2021-06-03 11:52:22 +0200

    JavaFx internationalization

--- Main.java
+++ Main.java
@@ -6,13 +6,27 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+import java.util.Locale;
+import java.util.ResourceBundle;
+
 public class Main extends Application {
+    private Locale locale = new Locale("en_UK");
+
+    public Locale getLocale(){
+        return locale;
+    }
+
+    public void setLocale(Locale locale){
+        this.locale = locale;
+    }
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("mainPage.fxml"));
+        //Locale locale = new Locale("en_UK");
+        ResourceBundle bundle = ResourceBundle.getBundle("resources.labelText", locale);
+        Parent root = FXMLLoader.load(getClass().getResource("mainPage.fxml"), bundle);
         primaryStage.setTitle("Football app");
-        primaryStage.setScene(new Scene(root, 1315, 855));
+        primaryStage.setScene(new Scene(root, 1340, 855));
         primaryStage.show();
     }
 
